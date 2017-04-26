3829de9997795541a645ab22bbfa1c72653a1f57
Jess Stewart <jeselistewart@gmail.com>
2017-04-26 09:20:08 -0700

    Update 17_3_zeros.py

--- 17_3_zeros.py
+++ 17_3_zeros.py
@@ -1,1 +1,11 @@
-
+def fib(n, cache):
+  if not cache:
+    cache = [0, 0, 1, 1]
+    
+  if n < len(cache):
+    return cache[n]
+  else:
+    cache.append(fib(n-1, cache) + fib(n-2, cache))
+    return cache[n]
+  
+print(fib(10, []))

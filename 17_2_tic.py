b382538377c280868aa474a372a7cfd99e297ef7
Jess Stewart <jeselistewart@gmail.com>
2017-04-26 09:59:51 -0700

    Update 17_2_tic.py

--- 17_2_tic.py
+++ 17_2_tic.py
@@ -1,15 +1,18 @@
 def has_winner(board):
   size = len(board)
+  
   # check rows
-  for row in board:
+  row = 0
+  while row < size:
     start = 0
     next = 1
     while next < size:
-      if row[start] != row[next]:
+      if board[row][start] != board[row][next]:
         break
       if next == size-1:
         return True
       next += 1
+    row += 1  
         
   # check columns
   column = 0
@@ -17,7 +20,7 @@ def has_winner(board):
     start = 0
     next = 1
     while next < size:
-      if board[column][start] != board[column][next]:
+      if board[start][column] != board[next][column]:
         break
       if next == size-1:
         return True

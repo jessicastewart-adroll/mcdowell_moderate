e6e3cdb126aae2e7845a785fadc03e03a3c1a1e7
Jess Stewart <jeselistewart@gmail.com>
2017-04-26 09:25:34 -0700

    Update 17_3_zeros.py

--- 17_3_zeros.py
+++ 17_3_zeros.py
@@ -1,11 +1,15 @@
 def fib(n, cache):
   if not cache:
-    cache = [0, 0, 1, 1]
+    cache = [0, 1, 1]
     
   if n < len(cache):
     return cache[n]
   else:
     cache.append(fib(n-1, cache) + fib(n-2, cache))
     return cache[n]
+    
+def fib_trailing_zeros(n):
+  fib_num = fib(n, [])
+  return len(str(fib_num)) - len(str(fib_num).strip('0'))
   
-print(fib(10, []))
+print(fib_trailing_zeros(15))

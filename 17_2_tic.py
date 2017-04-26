069f7d7c4dfa28bcbdfa6c97dfa464eee89c1d4d
Jess Stewart <jeselistewart@gmail.com>
2017-04-26 12:08:50 -0700

    Update 17_2_tic.py

--- 17_2_tic.py
+++ 17_2_tic.py
@@ -27,4 +27,32 @@ def has_winner(board):
       next += 1  
     column += 1  
   
-  # check diagonals
+  # \ diagonal
+  start = board[0][0]
+  next = 1
+  while next < size:
+    if board[next][next] != start:
+      break
+    if next == size-1:
+      return True
+    next += 1 
+    
+  # / diagonal
+  i = size-1
+  j = 0
+  start = board[i][j]
+  next = (-1, 1)
+  while i >= 0:
+    i = i+next[0]
+    j = j+next[1]
+    if board[i][j] != start:
+      break
+    if j == size-1:
+      return True
+      
+  return False    
+      
+test_one = [[None, None, None],
+[None, None, None],
+[None, None, None]]
+print(has_winner(test_one))

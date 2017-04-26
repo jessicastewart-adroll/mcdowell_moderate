6a55332164e606d4d9a425c3048d0bf805bea5d7
Jess Stewart <jeselistewart@gmail.com>
2017-04-26 08:59:56 -0700

    Update and rename 17_4.py to 17_4_max.py

---
+++ 17_4_max.py
@@ -0,0 +1,12 @@
+def get_max(a, b):
+  # assumes positive integers
+  counts = [[] for i in range(abs(a-b))]
+  pass
+  
+def get_max_neg(a, b):
+  # assumes positive or negative integers
+  pass
+
+def get_max_float(a, b):
+  # assumes positive or negative integers or floats
+  pass

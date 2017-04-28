c2e7ff1576d5de41f81f2f8257ef74f2dc9eba44
Jess Stewart <jeselistewart@gmail.com>
2017-04-28 09:04:48 -0700

    Update 17_4_max.py

--- 17_4_max.py
+++ 17_4_max.py
@@ -1,3 +1,4 @@
+# bitwise?
 def count_min(a, b):
 	buffer = [0 for i in range(a+b+1)]
 
@@ -28,7 +29,7 @@ def count_max(a, b):
 			return i
 		i -= 1
 
-print(count_max(10, 30)) # assumes positive integers
+print(count_max(10, 30))
 
 def bucket_max(a, b):
 	negative_values = []
@@ -53,8 +54,29 @@ def bucket_max(a, b):
 	if negative_values:
 		return -count_min(abs(a), abs(b))
 
-print(bucket_max(-10, -30)) # positive or negative integers
+print(bucket_max(-10, -30))
 
-def get_max_float(a, b):
-  # assumes positive or negative integers or floats
-  pass
+def max_floating_point(a, b):
+	a_len = str(a).split('.')
+	b_len = str(b).split('.')
+
+	if len(a_len) == 1 and len(b_len) == 1:
+		return bucket_max(a, b)
+	elif len(a_len) == 2 and len(b_len) == 2:
+		multiplier = count_max(len(a_len[1]), len(b_len[1]))
+	elif len(a_len) == 2:
+		multiplier = len(a_len[1])
+	else:
+		multiplier = len(b_len[1])
+
+	lookup = {}
+	mult_a = int(multiplier*10*a)
+	mult_b = int(multiplier*10*b)
+	lookup[mult_a] = a
+	lookup[mult_b] = b
+
+	int_max = bucket_max(mult_a, mult_b)
+
+	return lookup[int_max]
+
+print(max_floating_point(.6, -7.59))

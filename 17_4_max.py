e259e75e097a163781f440cc0e5cf3b0d52f6fb8
Jess Stewart <jeselistewart@gmail.com>
2017-04-27 18:44:30 -0700

    Update 17_4_max.py

--- 17_4_max.py
+++ 17_4_max.py
@@ -1,11 +1,59 @@
-def get_max(a, b):
-  # assumes positive integers
-  counts = [[] for i in range(abs(a-b))]
-  pass
-  
-def get_max_neg(a, b):
-  # assumes positive or negative integers
-  pass
+def count_min(a, b):
+	buffer = [0 for i in range(a+b+1)]
+
+	i = 0
+	while i < len(buffer):
+		if i == a or i == b:
+			buffer[i] += 1
+		i += 1
+
+	i = 0
+	while i < len(buffer):
+		if buffer[i]:
+			return i
+		i += 1
+
+def count_max(a, b):
+	buffer = [0 for i in range(a+b+1)]
+
+	i = 0
+	while i < len(buffer):
+		if i == a or i == b:
+			buffer[i] += 1
+		i += 1
+
+	i = len(buffer)-1
+	while i >= 0:
+		if buffer[i]:
+			return i
+		i -= 1
+
+print(count_max(10, 30)) # assumes positive integers
+
+def bucket_max(a, b):
+	negative_values = []
+	positive_values = []
+
+	if abs(a) != a:
+		negative_values.append(a)
+	else:
+		positive_values.append(a)
+
+	if abs(b) != b:
+		negative_values.append(b)
+	else:
+		positive_values.append(b)
+
+	if len(positive_values) == len(negative_values):
+		return positive_values[0]
+
+	if positive_values:
+		return count_max(a, b)
+
+	if negative_values:
+		return -count_min(abs(a), abs(b))
+
+print(bucket_max(-10, -30)) # positive or negative integers
 
 def get_max_float(a, b):
   # assumes positive or negative integers or floats

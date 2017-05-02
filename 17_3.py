521672b82ce29d25a71eea0ae41a8841bc24d5ca
Jess Stewart <jeselistewart@gmail.com>
2017-05-01 20:03:25 -0700

    Update 17_3.py

--- 17_3.py
+++ 17_3.py
@@ -1,23 +1,24 @@
-def fib(num):
+def fib(num, cache):
+	if num <= 2:
+		return cache[num]
+
+	if not cache.get(num):
+		cache[num] = fib(num-1, cache) + fib(num-2, cache)
+
+	return cache[num]
+
+def fib_trailing_zeros(num):
 	cache = {
 		0: 0,
 		1: 1,
 		2: 1
 	}
-	if num <= 2:
-		return cache[num]
-
-	if not cache.get(num):
-		cache[num] = fib(num-1) + fib(num-2)
-
-	return cache[num]
-
-def fib_trailing_zeros(num):
-	fib_num = fib(num)
+	fib_num = fib(num, cache)
 	count = 0
 
 	while not fib_num % 10:
 		count += 1
 		fib_num /= 10
-
 	return count
+
+fib_trailing_zeros(60)

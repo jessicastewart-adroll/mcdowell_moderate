54e466280ab011cc36228cdec651f3c5ec03c16d
Jess Stewart <jeselistewart@gmail.com>
2017-05-04 16:46:14 -0700

    Update 17_5.py

--- 17_5.py
+++ 17_5.py
@@ -1,14 +1,22 @@
-## pseudo-hits can only be used once
-## frequency table
-
 import random
+from collections import defaultdict
 
 class MasterMind(object):
 	COLORS = 'RYGB'
 	GUESS_LENGTH = 4
+	COLOR_CODES = {
+		'R':0,
+		'Y':1,
+		'G':2,
+		'B':3
+	}
 
 	def __init__(self):
 		self.solution = None
+
+	@classmethod
+	def get_code(cls, letter):
+		return cls.COLOR_CODES[letter]
 
 	def generate_solution(self):
 		solution = []
@@ -22,12 +30,15 @@ class MasterMind(object):
 		self.solution = solution
 
 	def guess(self, guess):
+		# debug
+		# print('\n', self.solution)
+		# print(guess)
+
 		result = {
 			'hits': 0,
 			'pseudo-hits': 0
 		}
-		pseudo_candidates = []
-		solution_candidates = []
+		freqency_table = [0]*MasterMind.GUESS_LENGTH
 
 		if len(guess) != MasterMind.GUESS_LENGTH:
 			return
@@ -40,22 +51,22 @@ class MasterMind(object):
 			if guess[i] == self.solution[i]:
 				result['hits'] += 1
 			else:
-				pseudo_candidates.append(guess[i])
-				solution_candidates.append(self.solution[i])
+				freqency_table[self.get_code(self.solution[i])] += 1
 			i += 1
 
-		for char in pseudo_candidates:
-			if char in solution_candidates:
+		i = 0
+		while i < MasterMind.GUESS_LENGTH:
+			if freqency_table[self.get_code(guess[i])] > 0 and guess[i] != self.solution[i]:
+				freqency_table[self.get_code(guess[i])] -= 1
 				result['pseudo-hits'] += 1
+			i += 1
 
 		return result
-
-
-	
-mm_1 = MasterMind()
-mm_1.generate_solution()
-print(mm_1.guess('RGGB'))
 
 mm_2= MasterMind()
 mm_2.generate_solution()
 print(mm_2.guess('RGBY'))
+print(mm_2.guess('RRRR'))
+print(mm_2.guess('XX'))
+print(mm_2.guess('BYGB'))
+print(mm_2.guess('ZZZZ'))

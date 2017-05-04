caa5f9052fc5966ccc52644eabb7c1d8e77e66f1
Jess Stewart <jeselistewart@gmail.com>
2017-05-04 08:51:13 -0700

    Update 17_3.py

--- 17_3.py
+++ 17_3.py
@@ -1,20 +1,39 @@
 def factorial_zeros(number):
-	if number <= 0:
+	if number < 5:
 		return 0
 
-	factorial = 1
-	i = 1
-	while i <= number:
-		factorial *= i
-		i += 1
+	if number == 5:
+		return 1
 
-	zeros = 0
-	while not factorial%10:
-		zeros += 1
-		factorial /= 10
+	counts = {
+		2: 1,
+		5: 1
+	}
+	factorial_number = 6
+	temp = factorial_number
 
-	return zeros
+	while factorial_number <= number:
+		while not temp%5:
+			temp /= 5
+			counts[5] += 1
+		while not temp%2:
+			temp /= 2
+			counts[2] += 1
+		factorial_number += 1
+		temp = factorial_number
+
+	# factorial check
+	# total = number
+	# while number > 1:
+	# 	number-=1
+	# 	total *= number
+	# print(total)
+
+	total_zeros = 0
+	total_zeros += min(counts[2], counts[5])
+
+	return total_zeros
 
 print(factorial_zeros(20))
-print(factorial_zeros(4))
-print(factorial_zeros(10))
+print(factorial_zeros(6))
+print(factorial_zeros(100))

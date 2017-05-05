862bb96025de93e8a612012a5161abfe678c8290
Jess Stewart <jeselistewart@gmail.com>
2017-05-04 20:01:27 -0700

    Update 17_6_smallest_swap.py

--- 17_6_smallest_swap.py
+++ 17_6_smallest_swap.py
@@ -1,18 +1,35 @@
+# check for already sorted
+# check for swap 1
+# quicksort
 def smallest_sort(array):
-	start = 0
-	n = len(array)
-	counts = [0]*n
-	end = n-1
+	i = 0
+	n = len(array) - 1
 
-	while start < end:
-		if array[start] <= array[start+1]:
-			counts[start+1] = counts[start]+1
-		if array[end-1] <= array[end]:
-			counts[end-1] = counts[end]+1
+	while i < len(array):
+	 	if array[i] > array[i+1]:
+	 		break
+ 		i += 1
 
-		start += 1
-		end -= 1
+	while n > 0:
+		if array[n-1] > array[n]:
+			break
+		n -= 1
 
-	print(counts)
+	smallest = min(array[i:n+1])
+	largest = max(array[i:n+1])	
 
-smallest_sort([1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19])
+	# debug
+	# print(smallest, largest)
+	# print(array[i:n+1])
+
+	i -= 1
+	while smallest < array[i] and i >= 0:
+		i -= 1
+
+	n += 1
+	while largest > array[n] and n < len(array):
+		n += 1
+
+	return (i+1, n-1)
+
+print(smallest_sort([1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]))

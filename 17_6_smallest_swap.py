afca40eca6bef4d6c368d67efc2dcce548f8ded2
Jessica Stewart <jessica.stewart@adroll.com>
2017-05-06 07:41:31 -0700

    Update 17_6_smallest_swap.py

--- 17_6_smallest_swap.py
+++ 17_6_smallest_swap.py
@@ -1,35 +1,31 @@
-# check for already sorted
-# check for swap 1
-# quicksort
+from heapq import *
+
 def smallest_sort(array):
+	if len(array) < 2:
+		return 
+
+	heap = array[:]
+	heapify(array)
 	i = 0
-	n = len(array) - 1
+	min_index = None
+	max_index = None
+	while heap:
+		element = heappop(heap)
+		if element != array[i]:
+			if min_index == None:
+				min_index = i
+			else:
+				max_index = i
+		i += 1 
 
-	while i < len(array):
-	 	if array[i] > array[i+1]:
-	 		break
- 		i += 1
+	if min_index == None:
+		return (0, 0)
 
-	while n > 0:
-		if array[n-1] > array[n]:
-			break
-		n -= 1
+	return (min_index, max_index)
 
-	smallest = min(array[i:n+1])
-	largest = max(array[i:n+1])	
-
-	# debug
-	# print(smallest, largest)
-	# print(array[i:n+1])
-
-	i -= 1
-	while smallest < array[i] and i >= 0:
-		i -= 1
-
-	n += 1
-	while largest > array[n] and n < len(array):
-		n += 1
-
-	return (i+1, n-1)
-
+print(smallest_sort([]))
+print(smallest_sort([1]))
+print(smallest_sort([2, 3]))
+print(smallest_sort([3, 2]))
+print(smallest_sort([2, 3, 0]))
 print(smallest_sort([1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]))
